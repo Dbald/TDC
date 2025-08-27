f73c80d1ac75536dc1e38d3b43f4972e9dc33a3f
Dbald <devinbaldwin315@gmail.com>
2025-08-26 19:33:22 -0700

    update vite

--- server/vite.ts
+++ server/vite.ts
@@ -5,6 +5,7 @@ import { createServer as createViteServer, createLogger } from "vite";
 import { type Server } from "http";
 import viteConfig from "../vite.config";
 import { nanoid } from "nanoid";
+import type { ServerOptions } from "vite";
 
 const viteLogger = createLogger();
 
@@ -20,9 +21,8 @@ export function log(message: string, source = "express") {
 }
 
 export async function setupVite(app: Express, server: Server) {
-  const serverOptions = {
+  const serverOptions: ServerOptions = {
     middlewareMode: true,
-    hmr: { server },
     allowedHosts: true,
   };
 
